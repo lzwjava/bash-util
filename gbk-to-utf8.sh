63ea322a7b2036d65022349b0d07b2b0a1d2135d
lzwjava <lzwjava@gmail.com>
2014-08-17 17:31:54 +0800

    remove unneed

--- gbk-to-utf8.sh
+++ gbk-to-utf8.sh
@@ -2,9 +2,9 @@
 dir1=src
 dir2=src2
 find $dir1 -type d -print | xargs -t -i mkdir $dir2/{} -p
-for filename in $(find $dir1 -type f)        # 'dir' should be changed...
+for filename in $(find $dir1 -type f)    
 do
-    destfile="src2/"$filename        # 'dir2' should also be changed...
+    destfile="src2/"$filename   
 		iconv -f GBK -t UTF-8 < $filename > $destfile
 		#echo $destfile
 done
